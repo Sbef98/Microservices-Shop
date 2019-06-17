944353ae83b0c7a769471b91e7c872f5274709a2
leona <leona@DESKTOP-HI5GDS6.homenet.telecomitalia.it>
2019-06-17 18:09:59 +0200

    ProvaTettone

---
+++ src/Tettone.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author leona
+ *
+ */
+public class Tettone {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
