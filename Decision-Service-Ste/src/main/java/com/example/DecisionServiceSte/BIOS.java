4089d90b56b7007daa4e55c0b8ccb69220c33656
Sbef98 <stoppy98@gmail.com>
2019-07-28 00:43:33 +0200

    Prototyping a pubblish/subscribe way

--- Decision-Service-Ste/src/main/java/com/example/DecisionServiceSte/BIOS.java
+++ Decision-Service-Ste/src/main/java/com/example/DecisionServiceSte/BIOS.java
@@ -18,53 +18,61 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Stefano Lugli;
  */
 @RestController
-public class BIOS { //basic input output service (nice joke i know)
-	HashMap<String,ServiceDetailsRequestModel> availableServices = new HashMap<String, ServiceDetailsRequestModel>();
-	//Hashtable has a big overhead due to sync features, but it's thread safe.
-	//TODO File servicesHistory; Files in java suck. Check it later  
-	
+public class BIOS { // basic input output service (nice joke i know)
+	HashMap<String, ServiceDetailsRequestModel> availableServices = new HashMap<String, ServiceDetailsRequestModel>();
+	// Hashtable has a big overhead due to sync features, but it's thread safe.
+	// TODO File servicesHistory; Files in java suck. Check it later
+
 	@Autowired
-	public BIOS() { //throws IOException{
-		//TODO servicesHistory = new File("servicesHistory.JSON");  //Used just for personal Info
-		//or maybe a tiny data base
+	public BIOS() { // throws IOException{
+		// TODO servicesHistory = new File("servicesHistory.JSON"); //Used just for
+		// personal Info
+		// or maybe a tiny data base
 	}
+
 	@PostConstruct
 	public void init() {
-		// possible TODO The surveillance thread used to check the correct services updates will be starting from here
-		//Not sure if i actually do need this
+		// possible TODO The surveillance thread used to check the correct services
+		// updates will be starting from here
+		// Not sure if i actually do need this
 	}
-	
-	@GetMapping(value="get-available-services",produces="application/json")
-	public String getConnectionsInfo()
-	{
-	    Iterator it = availableServices.entrySet().iterator();
-	    JSONObject returnValue = new JSONObject();
-	    while (it.hasNext()) {
-	        HashMap.Entry pair = (HashMap.Entry)it.next();
-	        returnValue.put((String) pair.getKey(), ((ServiceDetailsRequestModel) pair.getValue()).getServiceData());
-	    }
+
+	@GetMapping(value = "get-available-services", produces = "application/json")
+	public String getConnectionsInfo() {
+		Iterator it = availableServices.entrySet().iterator();
+		JSONObject returnValue = new JSONObject();
+		while (it.hasNext()) {
+			HashMap.Entry pair = (HashMap.Entry) it.next();
+			returnValue.put((String) pair.getKey(), ((ServiceDetailsRequestModel) pair.getValue()).getServiceData());
+		}
 		return returnValue.toString();
 	}
-	
-	
-	@PutMapping(value = "put", produces="application/json") //update services data
-	public String updateService(@RequestParam(value="serviceName") String serviceName, @RequestBody ServiceDetailsRequestModel requestServiceDetails) {
-		availableServices.put(serviceName, requestServiceDetails); 
-		String returnValue = requestServiceDetails.getType().compareToIgnoreCase("sensor") == 0 ? 
-							new String(requestServiceDetails.toString()) : //Returning the same string may be used to check that the communcation was correct!
-							DecisionMaker.takeDecision(requestServiceDetails, availableServices); //The way decision will be hadnled may vary
-		// TODO availableServices.get(serviceName); //Write the old ServiceName's value in the service history.
+
+	@PutMapping(value = "put", produces = "application/json") // update services data
+	public String updateService(@RequestParam(value = "serviceName") String serviceName,
+			@RequestBody ServiceDetailsRequestModel requestServiceDetails) {
+		availableServices.put(serviceName, requestServiceDetails);
+		String returnValue = requestServiceDetails.getType().compareToIgnoreCase("sensor") == 0
+				? new String(requestServiceDetails.toString())
+				: // Returning the same string may be used to check that the communcation was
+					// correct!
+				DecisionMaker.takeDecision(requestServiceDetails, availableServices); // The way decision will be
+																						// hadnled may vary
+		// TODO availableServices.get(serviceName); //Write the old ServiceName's value
+		// in the service history.
 		System.out.print("Put request for service status update from: ");
 		System.out.println(serviceName);
 		System.out.println("Answer: " + returnValue);
-		return returnValue; 
+		return returnValue;
 	}
-	//@GetMapping("get-history") TODO -> will return the services requests history since a given date
-	//The get mapping may be used by any kind of service or by the GUI
+
+	// @GetMapping("get-history") TODO -> will return the services requests history
+	// since a given date
+	// The get mapping may be used by any kind of service or by the GUI
 	@PreDestroy
 	public void destroy() {
-		//servicesHistory.close(); files do not close... ? 
-		//surveillance_thread.join(); (or kill it brutally who cares)
-		//TODO close all the services
+		// servicesHistory.close(); files do not close... ?
+		// surveillance_thread.join(); (or kill it brutally who cares)
+		// TODO close all the services
 	}
 }
--- Decision-Service-Ste/src/main/java/com/example/DecisionServiceSte/DecisionMaker.java
+++ Decision-Service-Ste/src/main/java/com/example/DecisionServiceSte/DecisionMaker.java
@@ -1,126 +1,178 @@
 package com.example.DecisionServiceSte;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class DecisionMaker 
-{
-	private static LinkedList<String> getAllSensorsNames(HashMap<String,ServiceDetailsRequestModel> availableServices)
-	{
+public class DecisionMaker {
+
+	private float mode(LinkedList<Float> numbers) {
+		int maxCount = 0, i, j;
+		float maxValue = 0;
+		for (i = 0; i < numbers.size(); i++) {
+			int count = 0;
+			for (j = 0; j < numbers.size(); ++j) {
+				if (numbers.get(j).compareTo(numbers.get(i)) == 0)
+					++count;
+			}
+			if (count > maxCount) {
+				maxCount = count;
+				maxValue = numbers.get(i);
+			}
+		}
+		return maxValue;
+	}
+
+	private static LinkedList<String> getAllSensorsNames(
+			HashMap<String, ServiceDetailsRequestModel> availableServices) {
 		LinkedList<String> names_list = new LinkedList<String>();
 		Iterator it = availableServices.entrySet().iterator();
-	    while (it.hasNext()) {
-	        HashMap.Entry pair = (HashMap.Entry)it.next(); 
-	        if(((ServiceDetailsRequestModel) pair.getValue()).getType().compareToIgnoreCase("sensor") == 0) {
-	        	names_list.add((String) pair.getKey()); 
-	        }
-	    }
+		while (it.hasNext()) {
+			HashMap.Entry pair = (HashMap.Entry) it.next();
+			if (((ServiceDetailsRequestModel) pair.getValue()).getType().compareToIgnoreCase("sensor") == 0) {
+				names_list.add((String) pair.getKey());
+			}
+		}
 		return names_list;
 	}
-	
-	private static float getMeasuredSensorData(String valueName, JSONObject measuredValues)
-	{
+
+	private static float getMeasuredSensorData(String valueName, JSONObject measuredValues) {
 		/*
-		 * valueName is the name of the measured value i am looking for while measured values are ALL the measured values by the sensor
-		 * in the form valueName : [measuredValues1, measuredValue2, ...]
+		 * valueName is the name of the measured value i am looking for while measured
+		 * values are ALL the measured values by the sensor in the form valueName :
+		 * [measuredValues1, measuredValue2, ...]
 		 */
 		float returnValue = 0;
 		int i = 0;
-		Map<String,Object> values = measuredValues.toMap();
-		//Now i have a map where the key is the name of the measured value from the sensor
+		Map<String, Object> values = measuredValues.toMap();
+		// Now i have a map where the key is the name of the measured value from the
+		// sensor
 		Iterator it = values.entrySet().iterator();
-	    while (it.hasNext()) {
-	    	Map.Entry pair = (Map.Entry)it.next();
-	    	//I need to check if this is the value i was looking for
-	    	if(valueName.compareToIgnoreCase((String) pair.getKey()) == 0) {
-	    		//This is what i was looking for!
-	    		for(Object value : ((JSONArray) pair.getValue())) {
-	    			i++;
-	    			returnValue += (float) value;
-	    		}
-	    		return returnValue/i; //return the mean of the measured values
-	    	}
-	    	
-	    }
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry) it.next();
+			// I need to check if this is the value i was looking for
+			if (valueName.compareToIgnoreCase((String) pair.getKey()) == 0) {
+				// This is what i was looking for!
+				for (Object value : ((JSONArray) pair.getValue())) {
+					i++;
+					returnValue += (float) value;
+				}
+				return returnValue / i; // return the mean of the measured values
+			}
+
+		}
 		return returnValue;
 	}
-	
-	private static float getWantedSensorValue()
-	{
-		float returnValue = 0;
-		
-		return returnValue;
-	}
-	
-	private static float computeCorrection()
-	{
-		float returnValue = 0;
-		
-		return returnValue;
-	}
-	
-	private static JSONArray ComputeAnswer(String key, JSONObject value, HashMap<String,ServiceDetailsRequestModel> availableServices)
-	{
+
+	private static float getWantedSensorValue(String valueName, JSONArray references, JSONObject suggestedValues,
+			HashMap<String, ServiceDetailsRequestModel> availableServices) {
 		/*
-		 * Key is the sensor's name
-		 * value is needed_value : [references,...]
+		 * Will be using modal value. This is because finding an average value may not
+		 * make happy anybody. So let's just choose the most wanted one, the democratic
+		 * way
+		 */
+		float returnValue = 0;
+		LinkedList<Float> values = new LinkedList<Float>();
+		for(Object reference : references) {
+			JSONObject wanted;
+			try {
+				if(availableServices.get((String) reference).getType().compareToIgnoreCase("sensor") == 0)
+					wanted = availableServices.get((String) reference).getData().getJSONObject("wanted"); 
+				else 
+					wanted = availableServices.get((String) reference).getData().getJSONObject("suggested");
+			}catch(JSONException e){
+				System.out.println(e);
+				continue;
+			}
+			//TODO trova nei wanted quello che ti serve e aggiungilo. Buonanotepper oggi
+		}
+		return returnValue;
+	}
+
+	private static float computeCorrection() {
+		float returnValue = 0;
+
+		return returnValue;
+	}
+
+	private static JSONArray ComputeAnswer(String key, JSONObject value,
+			HashMap<String, ServiceDetailsRequestModel> availableServices, JSONObject suggestedValues) {
+		/*
+		 * Key is the sensor's name value is needed_value : [references,...]
 		 */
 		JSONArray returnValue = new JSONArray();
 		ServiceDetailsRequestModel sensorData = availableServices.get(key);
-		if(sensorData == null) 
-			System.out.println("Servizio "+ key +" non disponibile");
+		if (sensorData == null)
+			System.out.println("Servizio " + key + " non disponibile");
 		else {
-			Map<String,Object> needed_data = value.toMap(); //Now i have a map where the key is the needed_value and the body is the JSONArray of references
+			Map<String, Object> needed_data = value.toMap(); // Now i have a map where the key is the needed_value and
+																// the body is the JSONArray of references
 			Iterator it = needed_data.entrySet().iterator();
-		    while (it.hasNext()) {
-		        Map.Entry pair = (Map.Entry)it.next(); //We can use the key to retrive the right sensor measured value from 
-		        try {
-		        	float measuredData = getMeasuredSensorData((String) pair.getKey(), sensorData.getData().getJSONObject("measured")); //This is the value measured by this sensor
-		        }catch(JSONException e) {
-		        	System.out.println("Missing measured field in sensor " + key);
-		        }
-		        float wantedValue = getWantedSensorValue(); //TODO
-		        float correction = computeCorrection(); //TODO
-		        returnValue.put(correction);
-		    }
+			while (it.hasNext()) {
+				Map.Entry pair = (Map.Entry) it.next(); // We can use the key to retrieve the right sensor measured
+														// value
+														// from
+				try {
+					float measuredData = getMeasuredSensorData((String) pair.getKey(),
+							sensorData.getData().getJSONObject("measured")); // This is the value measured by this
+																				// sensor
+				} catch (JSONException e) {
+					System.out.println("Missing measured field in sensor " + key);
+				}
+				float wantedValue = getWantedSensorValue((String) pair.getKey(), (JSONArray) pair.getValue(),
+						suggestedValues, availableServices);
+				// I am passing the ValueName i am evaluating, from which services i should get
+				// the suggested value it should actually be, the suggested vlues from this
+				// actuator
+				float correction = computeCorrection(); // TODO
+				returnValue.put(correction);
+			}
 		}
 		return returnValue;
 	}
-	
-	
-	public static String takeDecision(ServiceDetailsRequestModel applicantInfo, HashMap<String,ServiceDetailsRequestModel> availableServices)
-	{
-		
+
+	public static String takeDecision(ServiceDetailsRequestModel applicantInfo,
+			HashMap<String, ServiceDetailsRequestModel> availableServices) {
+
 		JSONObject returnValue = new JSONObject();
-		Map<String,Object> needed_sensors;
+		JSONObject suggestedValues;
+		Map<String, Object> needed_sensors;
 		try {
-			needed_sensors = ((JSONObject) applicantInfo.getData().get("needed_sensors")).toMap();
-		} catch(JSONException e){
+			needed_sensors = (applicantInfo.getData().getJSONObject("needed_sensors")).toMap();
+		} catch (JSONException e) {
 			return "Wrong protocol used.";
 		}
-		
+
+		try {
+			suggestedValues = (applicantInfo.getData().getJSONObject("suggested"));
+		} catch (JSONException e) {
+			suggestedValues = new JSONObject(); // WHich is empty
+		}
+
 		Iterator it = needed_sensors.entrySet().iterator();
-	    while (it.hasNext()) {
-	        Map.Entry pair = (Map.Entry)it.next(); 
-	        String key = (String) pair.getKey();
-	        JSONObject value = (JSONObject) pair.getValue();
-	        if(key.compareToIgnoreCase("*") != 0) {
-	        	returnValue.put(key, ComputeAnswer(key,value, availableServices)); //TODO add the version with the suggested values
-	        }
-	        else {
-	        	for(String key2 : getAllSensorsNames(availableServices)) {
-	        		returnValue.put((String) pair.getKey(), ComputeAnswer(key2,value,availableServices));
-	        	}
-	        }
-	        
-	    }
-	    
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry) it.next();
+			String key = (String) pair.getKey();
+			JSONObject value = (JSONObject) pair.getValue();
+			if (key.compareToIgnoreCase("*") != 0) {
+				returnValue.put(key, ComputeAnswer(key, value, availableServices, suggestedValues));
+			} else {
+				for (String key2 : getAllSensorsNames(availableServices)) {
+					returnValue.put((String) pair.getKey(),
+							ComputeAnswer(key2, value, availableServices, suggestedValues));
+				}
+			}
+
+		}
+
 		return returnValue.toString();
 	}
 }
