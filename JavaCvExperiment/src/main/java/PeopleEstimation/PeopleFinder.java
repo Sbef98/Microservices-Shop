1502816579c70c6b4d79b684e73dcc685575925c
Sbef98 <stoppy98@gmail.com>
2019-08-10 18:14:41 +0200

    JavaCv experimenting

---
+++ JavaCvExperiment/src/main/java/PeopleEstimation/PeopleFinder.java
@@ -0,0 +1,10 @@
+package PeopleEstimation;
+
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+
+public class PeopleFinder 
+{
+	String videoLink;
+	FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber("/dev/video0");
+	
+}
