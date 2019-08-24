7feed8c87f70d7b957feaedf3b5a2c1c2de07ac9
LeoMirots <leonardo.mirotti@gmail.com>
2019-08-24 20:20:01 +0200

    Added communication class JavaDoc
    Added also first README PDF file

--- web-service-example/src/main/java/org/example/Communication.java
+++ web-service-example/src/main/java/org/example/Communication.java
@@ -8,7 +8,27 @@ import kong.unirest.UnirestException;
  * response = Communication.put(url, serviceURI, servicePort, type, getGetMapping(), getPutMapping(),
  * 								groupID, description, getValues(), getWanted(), getNeeded_sensors());
  */
+/**
+ *Class to describe HTTP communication model between services of the system.
+ */
 public class Communication {
+	/**
+	 * Function to process the PUT request building the JSONObject 
+	 * 
+	 * @param url Decision service's url: the target of PUT message
+	 * @param serviceURI address of the actual service
+	 * @param serverPort port listening at "serviceURI" address
+	 * @param type type of service: sensor or actuator
+	 * @param getMapping GET functions list to use with this service to execute a data request. Optional
+	 * @param putMapping PUT functions list of this service. Optional
+	 * @param groupID service belonging group
+	 * @param description general info
+	 * @param values Actual measured values
+	 * @param wanted Ideal target values suggested
+	 * @param needed_sensors USED ONLY FOR ACTUATORS: model to describe the comparison beetwen which services are needed to execute actions   
+	 * @return TO SENSORS: resends the received message, TO ACTUATOR: return a JSONObject with values' delta
+	 * @throws UnirestException
+	 */
 	protected static String put(String url, String serviceURI, int serverPort, String type,
 								String getMapping, String putMapping, String groupID, String description,
 								JSONObject values, JSONObject wanted, JSONObject needed_sensors) throws UnirestException
@@ -27,6 +47,14 @@ public class Communication {
 		msg.put("needed_sensors", needed_sensors.toString());
 		return sendJSONObject(msg,url);		
 	}
+	
+	/**
+	 * Function to tell to the decision service that this service will be shut down
+	 * 
+	 * @param url target address of message: Decision service URL
+	 * @param groupID service belonging group
+	 * @throws UnirestException
+	 */
 	protected static void close(String url, String groupID) throws UnirestException
 	{
 		JSONObject msg = new JSONObject();
@@ -35,6 +63,13 @@ public class Communication {
 		sendJSONObject(msg, url);
 	}
 	
+	/**
+	 * Internal service function to execute a HTTP PUT request on a target URL
+	 * 
+	 * @param msg JSONObject to send
+	 * @param url target address
+	 * @return body of the PUT response message
+	 */
 	private static String sendJSONObject(JSONObject msg, String url)
 	{
 		HttpResponse<String> response = Unirest.put(url) //Gotta understand this first
