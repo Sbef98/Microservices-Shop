a67b4f58af5747f525c5b9dede72e80dc3665812
LeoMirots <leonardo.mirotti@gmail.com>
2019-09-14 16:39:36 +0200

    Removed Warnings on HistoryTracker

--- Decision-Service/src/main/java/history/HistoryTracker.java
+++ Decision-Service/src/main/java/history/HistoryTracker.java
@@ -1,14 +1,10 @@
 package history;
 
-import java.sql.Connection;
 import java.util.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.Iterator;
-import java.util.LinkedList;
-
 import org.json.JSONArray;
 
 import com.example.DecisionServiceSte.ServiceDetailsRequestModel;
@@ -16,7 +12,7 @@ import com.example.DecisionServiceSte.ServiceDetailsRequestModel;
 public class HistoryTracker {
 	public static void main(String[] args) throws SQLException {
 	DatabaseConnection dbConn = new DatabaseConnection();
-	Connection conn = DatabaseConnection.DBConnection();
+	DatabaseConnection.DBConnection();
 	DatabaseConnection.AssignID();
 	ResultSet res = DatabaseConnection.ExecQuery("SELECT * FROM Services");
 	System.out.print(res.toString());
